4a957b2691d635888b2b7b062d2402f56bd1a9dd
Ryzen-thor <aman.friend2016@gmail.com>
2021-02-26 13:05:38 +0530

    Food blog

---
+++ public/script.js
@@ -0,0 +1,14 @@
+const burger = document.getElementById('burger');
+const menu = document.getElementById('menu');
+let flag=1;
+burger.addEventListener('click',()=>{
+    
+    if(menu.classList.contains('hidden')){
+        menu.classList.remove('hidden');
+    
+    }else{
+        menu.classList.add('hidden');
+        
+    }
+    
+})
